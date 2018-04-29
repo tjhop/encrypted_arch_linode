b6e63955333948906bd5847eb3c730d4df98ce47
TJ Hoplock <t.hoplock@gmail.com>
2018-04-29 15:31:47 -0400

    add script to use Linode APIv4 to create and prep linode
    ...along with an example configuration file to specify options to use in
    the script that get passed through to the API.

---
+++ create.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+"""
+A python3 wrapper to create and prep a Linode for use with either the encrypted Arch or encrypted Debian scripts:
+https://github.com/tjhop/encrypted_arch_linode
+https://github.com/tjhop/encrypted_debian_linode
+"""
+
+from linode import LinodeClient
+import yaml
+import time
+import calendar
+
+# retreive api token from config file and initialize client
+with open('config.yaml', 'r') as f:
+    config = yaml.safe_load(f)
+
+token = config.get('api').get('api-token')
+client = LinodeClient(token)
+
+# get necessary variables
+linode_type = config.get('linode').get('plan', 'g5-nanode-1')
+region = config.get('linode').get('region', 'us-east')
+linode_label = config.get('linode').get('label', str(calendar.timegm(time.gmtime())))
+linode_group = config.get('linode').get('group')
+config_label = config.get('linode').get('config_profile').get('label')
+
+# create empty linode
+print("* Creating Linode -> {}".format(linode_label))
+l = client.linode.create_instance(ltype=linode_type, region=region, label=linode_label, group=linode_group)
+disk_size = vars(l.specs)['disk'] - 512
+
+print('* Making disks')
+# configure disks
+while l.status not in ('offline', 'running'):
+    time.sleep(5)
+    print("~ waiting on Linode status... ({})".format(l.status))
+print('* Creating boot disk')
+disk_sda = l.create_disk(size=256, label='Boot', filesystem='raw')
+
+# TODO: Change polling to check disk status for `disk_sda` not in 'not ready'
+while disk_sda.status not in ('ready'):
+    time.sleep(5)
+    print("~ waiting on disk 'sda' status... ({})".format(disk_sda.status))
+print('* Creating swap disk')
+disk_sdb = l.create_disk(size=256, label='Swap', filesystem='raw')
+
+while disk_sdb.status not in ('ready'):
+    time.sleep(5)
+    print("~ waiting on disk 'sdb' status... ({})".format(disk_sdb.status))
+print('* Creating system disk')
+disk_sdc = l.create_disk(size=disk_size, label='System', filesystem='raw')
+
+# create configuration profile
+while disk_sdc.status not in ('ready'):
+    time.sleep(5)
+    print("~ waiting on disk 'sdc' status... ({})".format(disk_sdc.status))
+helpers_dict = {'updatedb_disabled': False,
+                'distro': False,
+                'modules_dep': False,
+                'network': False,
+                'devtmpfs_automount': False }
+print('* Creating config profile')
+l.create_config(kernel='linode/grub2',
+                label=config_label,
+                devices=l.disks,
+                helpers=helpers_dict)
+
+time.sleep(3)
+
+# reboot into rescue mode
+while l.status not in ('offline', 'running'):
+    time.sleep(5)
+    print("~ waiting on Linode status... ({})".format(l.status))
+
+# call to `rescue` requires disk ids in form of a list, so let's make one
+
+disks = []
+for d in l.disks:
+    disks.append(d.id)
+print("* Rebooting into rescue mode with the following disks:\n{}".format(disks))
+l.rescue(*disks)
+
+print("Linode's IP addresses:\nIPv4 -> {}\nIPv6 -> {}".format(*l.ipv4, l.ipv6))
